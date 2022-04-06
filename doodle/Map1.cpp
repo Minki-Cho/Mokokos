549c12cdbad544f4ae529813c65f605a70c22dd3
truth0600 <minji.cho@digipen.edu>
2022-04-06 20:07:29 +0900

    Update Map1.cpp

--- doodle/Map1.cpp
+++ doodle/Map1.cpp
@@ -31,17 +31,41 @@ void Map1::wall_collision()
 	{
 		playMoving_x += 3.5;
 	}
-    if ((playMoving_y > 175 && playMoving_x > 63 ) || (playMoving_y > 175 && playMoving_x < -133)) // stair
+    if (playMoving_x > 277) // right wall
+    {
+        playMoving_x -= 3.5;
+    }
+    if (playMoving_y > 340) // up wall
+    {
+        playMoving_y -= 3.5;
+    }
+    if (playMoving_y < -322 && playMoving_x < -77 ) //down wall, left
+    {
+        playMoving_y += 3.5;
+    }
+    if (playMoving_y < -322 && playMoving_x > 10 ) //down wall, right
+    {
+        playMoving_y += 3.5;
+    }
+    if ((playMoving_y < 233 && playMoving_y > 175 && playMoving_x > 63 )) //right stair up
 	{
 		playMoving_y -= 3.5;
 	}
-	if (playMoving_x > 277) // right wall
-	{
-		playMoving_x -= 3.5;
+    if (playMoving_y < 236 && playMoving_y > 175 && playMoving_x > 63) //right stair down
+    {
+        playMoving_y += 3.5;
+    }
+    if (playMoving_y < 233 && playMoving_y > 175 && playMoving_x < -140) // left stair up
+    {
+        playMoving_y -= 3.5;
 	}
+    if (playMoving_y < 236 && playMoving_y > 175 && playMoving_x < -140) //left stair down
+    {
+        playMoving_y += 3.5;
+    }
 }
 
 void Map1::Map1_Door()
 {
-
+    //if (playMoving_x < -340 && playMoving_y < )
 }
